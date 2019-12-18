0a12c22798c32584c5ffe5eb73fdb0fc07cc15a5
Vladislav Prusakov <spectraldragonchannel@gmail.com>
2019-12-18 12:57:48 +0300

    Add lint

---
+++ SwiftDILint/SwiftDILint/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftDILint
+//
+//  Created by Vladislav Prusakov on 18.12.2019.
+//  Copyright © 2019 Vladislav Prusakov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
